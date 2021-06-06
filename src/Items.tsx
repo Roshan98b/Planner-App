21a3dba4d86f17a105d1ef4c9809b42595a9f6fe
Roshan Badrinath <roshan98b@gmail.com>
2021-06-06 22:06:38 +0530

    Started Boards

---
+++ src/Items.tsx
@@ -0,0 +1,26 @@
+export const items = [
+  {
+    taskName: "Task 1",
+    date: "26-01-2021",
+    initial: "RB",
+    name: "Roshan Badrinath",
+  },
+  {
+    taskName: "Task 2",
+    date: "26-02-2021",
+    initial: "BK",
+    name: "B Harsha Kashyap",
+  },
+  {
+    taskName: "Task 3",
+    date: "26-03-2021",
+    initial: "SL",
+    name: "Sagnik Lahiri",
+  },
+  {
+    taskName: "Task 4",
+    date: "26-04-2021",
+    initial: "NM",
+    name: "Noob Man",
+  },
+];
