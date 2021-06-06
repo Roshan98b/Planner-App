d62fd48495446c494e28efc6337ec6858109918d
HarshaKashyap <hakashya@microsoft.com>
2021-06-06 22:06:36 +0530

    Reduced calendar bugs

--- src/views/gridView.tsx
+++ src/views/gridView.tsx
@@ -1,4 +1,5 @@
 
+import { DefaultButton, IIconProps, Label, Stack } from '@fluentui/react'
 import React from 'react'
 import Cell from './cell'
 import './gridView.css'
@@ -16,21 +17,33 @@ const GridView = () => {
             {rowElemets}
         </div>)
     }
+
+    const left: IIconProps = { iconName: 'ChevronLeftSmall' };
+    const right: IIconProps = { iconName: 'ChevronRightSmall' };
     return (
         <div className="calendarView">
-            <div>
-                <div className="weekdays">
-                    <div className="weekday">Sunday</div>
-                    <div className="weekday">Monday</div>
-                    <div className="weekday">Tuesday</div>
-                    <div className="weekday">Wednesday</div>
-                    <div className="weekday">Thursday</div>
-                    <div className="weekday">Friday</div>
-                    <div className="weekday">Saturday</div>
+            <div className="childCalendar">
+                <div className="monthControl">
+                    <Stack horizontal>
+                        <DefaultButton iconProps={left}></DefaultButton>
+                        <Label>Aug, 2021</Label>
+                        <DefaultButton iconProps={right}></DefaultButton>
+                    </Stack>
                 </div>
-            </div>
-            <div className="col">
-                {colElements}
+                <div>
+                    <div className="weekdays">
+                        <div className="weekday">Sunday</div>
+                        <div className="weekday">Monday</div>
+                        <div className="weekday">Tuesday</div>
+                        <div className="weekday">Wednesday</div>
+                        <div className="weekday">Thursday</div>
+                        <div className="weekday">Friday</div>
+                        <div className="weekday">Saturday</div>
+                    </div>
+                </div>
+                <div className="col">
+                    {colElements}
+                </div>
             </div>
         </div>
     )
